5517ba5ec0c23086bbe17969215cafac47633324
ryohei-ochi-fr <ryohei.ochi@futurerays.biz>
2025-02-10 05:44:03 +0900

    お問い合わせフォームコンポーネントを追加し、ブログにリンクを更新

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ['新着情報', '労働組合とは', '2025年 春闘', '沿革'] as const
+export const CATEGORIES = ['新着情報', '労働組合とは', '2025年 春闘', '沿革', 'Contact'] as const
